4af7acec2d87861a2107fbd3db74d746992bdd84
Ariel O. Barria <ariel.barria@gmail.com>
2013-10-21 14:49:52 -0500

    add try-except

--- fscheck.py
+++ fscheck.py
@@ -14,18 +14,25 @@ def verify_uid(path, user, group, verbose):
             uid = os.stat(path_down).st_uid
             gid = os.stat(path_down).st_gid
 
-            if pwd.getpwuid(uid).pw_name == user:
-                status_u = "EQUAL"
-            else:
-                status_u = "NOT EQUAL"
+            try:
+                if pwd.getpwuid(uid).pw_name == user:
+                    status_u = "EQUAL"
+                else:
+                    status_u = "NOT EQUAL"
+            except KeyError:
+                print "Error: unknown uid ['%s']. ['%s']" % (uid, path_down)
+                sys.exit(2)
 
             mu = " --- [user = %i->%s] -- %s " % \
                  (uid, pwd.getpwuid(uid).pw_name, status_u)
-
-            if grp.getgrgid(gid).gr_name == group:
-                status_g = "EQUAL"
-            else:
-                status_g = "NOT EQUAL"
+            try:
+                if grp.getgrgid(gid).gr_name == group:
+                    status_g = "EQUAL"
+                else:
+                    status_g = "NOT EQUAL"
+            except KeyError:
+                print "Error: unknown gid ['%s']. ['%s']" % (gid, path_down)
+                sys.exit(2)
 
             mg = " --- [group = %i->%s] -- %s " % \
                  (gid, grp.getgrgid(gid).gr_name, status_g)
@@ -74,24 +81,58 @@ if __name__ == '__main__':
         path = raw_input("Input path - Default[.] ")
 
     if user == None:
-        user = raw_input(m1)
-        if user.isalpha():
-            user = pwd.getpwuid(os.getuid()).pw_name
+        error = 1
+        while(error):
+            user = raw_input(m1)
+            if user.isalpha() or user == "":
+                user = pwd.getpwuid(os.getuid()).pw_name
+                error = 0
+            else:
+                try:
+                    pwd.getpwnam(user)
+                    error = 0
+                except KeyError:
+                    print "Error: unknown user ['%s']" % user
 
     if group == None:
-        group = raw_input(m2)
+        error = 1
+        while(error):
+            group = raw_input(m2)
+            if group.isalpha() or group == "":
+                group = grp.getgrgid(os.getgid()).gr_name
+                error = 0
+            else:
+                try:
+                    grp.getgrnam(group)
+                    error = 0
+                except KeyError:
+                     print "Error: unknown group ['%s']" % group
 
     if path == None or path == "":
         path = "."
     else:
-        path = path.strip()
-    if os.path.exists(path) == False or path != ".":
-        print "No such directory [%s]." % path
 
-    if user.isalpha() or user == "":
-        user = pwd.getpwuid(os.getuid()).pw_name
-    if group.isalpha() or group == "":
-        group = grp.getgrgid(os.getgid()).gr_name
+        path = path.strip()
+    if os.path.exists(path) == False and path != ".":
+        print "No such directory [%s]." % path
+        sys.exit(2)
+
+    #if user.isalpha() or user == "":
+    #    user = pwd.getpwuid(os.getuid()).pw_name
+    #if group.isalpha() or group == "":
+    #    group = grp.getgrgid(os.getgid()).gr_name
+
+    #try:
+    #    pwd.getpwnam(user)
+    #except KeyError:
+    #    print "Error: unknown user ['%s']" % user
+    #    sys.exit(2)
+
+    #try:
+    #    pwd.getpwnam(group)
+    #except KeyError:
+    #    print "Error: unknown group ['%s']" % user
+    #    sys.exit(2)
 
     verify_uid(path, user, group, verbose)
     print "Complete review."
